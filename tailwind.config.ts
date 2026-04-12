5857ef0c5fce99329d8a2e7ac70e1ab8185fc450
Lucika Jean Baptiste <lucika_jeanbaptiste@yahoo.fr>
2026-04-12 14:49:58 -0400

    feat: initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,79 @@
+import type { Config } from 'tailwindcss'
+
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+export default {
+  darkMode: ['class'],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        green: {
+          500: "#24AE7C",
+          600: "#152432",
+        },
+        blue: {
+          500: "#7985EC",
+          600: "#152432",
+        },
+        red: {
+          500: "#f37877",
+          600: "#3E1716",
+          700: "#f24E43",
+        },
+        light: {
+          200: "#E8E9E9",
+        },
+        dark: {
+          200: "#0D0F10",
+          300: "#131619",
+          400: "#1A1D21",
+          500: "#363A3D",
+          600: "#76828D",
+          700: "#ABB8CA"
+        },
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+      backgroundImage: {
+        appointments: "url('/assets/images/appointments-bg.png')",
+        pending: "url('/assets/images/pending-bg.png')",
+        cancelled: "url('/assets/images/cancelled-bg.png')",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+        "caret-blink": {
+          "0%,70%,100%": { opacity: "1" },
+          "20%,50%": { opacity: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "caret-blink": "caret-blink 1.25s ease-out infinite",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
